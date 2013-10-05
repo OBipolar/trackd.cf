0c33bbcedc1b3ac69cb6a79ccea2e0014861e770
shiftynick <nick@redtreelabs.com>
2013-10-05 18:19:17 -0400

    sql tables

--- WebApp/Trackd/Trackd/Controllers/HomeController.cs
+++ WebApp/Trackd/Trackd/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,18 @@ namespace Trackd.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.WhatEverVAriable = "siomething";
+            IList<UserMetricEntry> list = new List<UserMetricEntry>();
+            list.Add(new UserMetricEntry { Id = 1, EntryTimestamp = DateTime.Now, Metric = new Metric { Id = 1, MetricType = new MetricType { Id = 1, Name = "FreeText" }, Name = "Poop" }, NumericalValue = 0, TextValue = "green", UserId = 1 });
+            list.Add(new UserMetricEntry { Id = 1, EntryTimestamp = DateTime.Now, Metric = new Metric { Id = 1, MetricType = new MetricType { Id = 1, Name = "FreeText" }, Name = "Poop" }, NumericalValue = 0, TextValue = "green", UserId = 1 });
+            list.Add(new UserMetricEntry { Id = 1, EntryTimestamp = DateTime.Now, Metric = new Metric { Id = 1, MetricType = new MetricType { Id = 1, Name = "FreeText" }, Name = "Poop" }, NumericalValue = 0, TextValue = "green", UserId = 1 });
+            list.Add(new UserMetricEntry { Id = 1, EntryTimestamp = DateTime.Now, Metric = new Metric { Id = 1, MetricType = new MetricType { Id = 1, Name = "FreeText" }, Name = "Poop" }, NumericalValue = 0, TextValue = "green", UserId = 1 });
+            ViewBag.EntryList = list;
             return View();
         }
     }
